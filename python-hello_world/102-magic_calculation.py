d181fe571c49777d9822dc386607408b11e8de83
Khaalid245 <k.abdillahi@alustudent.com>
2023-10-31 12:12:47 -0700

    102-magic_calculation.py

--- python-hello_world/102-magic_calculation.py
+++ python-hello_world/102-magic_calculation.py
@@ -4,3 +4,10 @@ def magic_calculation(a, b):
     result = result ** a  # LOAD_FAST 0 (a) + BINARY_POWER
     result = result + b  # LOAD_FAST 1 (b) + BINARY_ADD
     return result  # RETURN_VALUE
+
+# Example usage
+a = 3
+b = 5
+result = magic_calculation(a, b)
+print("Result:", result)
+
