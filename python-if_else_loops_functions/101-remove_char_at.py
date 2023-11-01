d7cb79d0283a545b25b7041a65d4cb357dc0f126
Khaalid245 <k.abdillahi@alustudent.com>
2023-11-01 02:29:56 -0700

    101-remove_char_at.py

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
+def remove_char_at(s, n):
+    # Check for invalid indices and return the original string
+    if n < 0 or n >= len(s):
+        return s
+    # Remove the character at index n by slicing the string
+    return s[:n] + s[n+1:]
 
-def remove_char_at(str, n):
-    if n < 0 or n >= len(str):
-        return str
-    return str[:n] + str[n+1:]
-
-print(remove_char_at("Best School", 3))
-print(remove_char_at("Chicago", 2))
-print(remove_char_at("C is fun!", 0))
-print(remove_char_at("School", 10))
-print(remove_char_at("Python", -2))
+# Test cases
+print(remove_char_at("Chicago", 3))  # Output: "Chcago"
+print(remove_char_at("Chicago", 15))  # Output: "Chicago"
+print(remove_char_at("", 4))  # Output: ""
+print(remove_char_at("Chicago", -3))  # Output: "Chicago"
+print(remove_char_at("Chicago", 0))  # Output: "hicago"
 
