bb98f2fa079eebf6c10438a230b645dda0719fee
Khaalid245 <k.abdillahi@alustudent.com>
2023-11-01 03:06:19 -0700

    2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import sys
 
-if __name__ == "__main__":
+if __name__ == "__main":
     args = sys.argv[1:]
     num_args = len(args)
 
@@ -9,3 +9,4 @@ if __name__ == "__main__":
     
     for i, arg in enumerate(args, start=1):
         print(f"{i}: {arg}")
+
