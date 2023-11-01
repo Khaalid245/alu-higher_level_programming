0156b67d2a3fd8e602df238956346b73b320d9b8
Khaalid245 <k.abdillahi@alustudent.com>
2023-11-01 02:19:55 -0700

    100-print_tebahpla.py

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,5 +3,5 @@
 for char in range(ord('z'), ord('a') - 1, -1):
     print(chr(char) if (char - ord('z')) % 2 == 0 else chr(char).upper(), end='')
 
-print()  # Add a newline after printing the characters
+print()  # smile mirror
 
