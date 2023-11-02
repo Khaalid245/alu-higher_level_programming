72e9e52cb4c47481a26702407ac403814b4d092a
Khaalid245 <k.abdillahi@alustudent.com>
2023-11-02 08:01:25 -0700

    100-print_tebahpla.py

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-
-for char in range(ord('z'), ord('a') - 1, -1):
-    print(chr(char) if (char - ord('z')) % 2 == 0 else chr(char).upper(), end='')
-
-print()  # smile mirror
+for char in range(ord('z'), ord('Z') - 1, -1):
+    if (ord('a') <= char <= ord('z')) or (ord('A') <= char < ord('Z')):
+        if char % 2 != 0:
+            print("{}".format(chr(char).upper()), end='')
+        else:
+            print("{}".format(chr(char).lower()), end='')
 
