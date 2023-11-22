cf2fcfae8f39951d71c5fbe9c27253ca9c262e32
Khaalid245 <k.abdillahi@alustudent.com>
2023-11-22 03:43:08 -0800

    100-magic_string.py

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
-"""Defines a locked class."""
-
-
-class LockedClass:
-    """
-    prevent the user from instantiating new lockedClass attributes
-    for anything but attributes called 'first_namer'.
-    """
-
-    __slots__ = ["first_name"]
+def magic_string():
+    
+        return f"BestSchool".split(",")    
