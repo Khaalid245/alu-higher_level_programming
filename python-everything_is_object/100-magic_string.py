69c480943a21164667f17124ac354641ae88e3b2
Khaalid245 <k.abdillahi@alustudent.com>
2023-11-24 09:44:52 -0800

    100-magic_string.py

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    
-        return f"BestSchool".split(",")    
+    magic_string.n = getattr(magic_string, 'n', 0) + 1
+    return ("Holberton, " * (magic_string.n - 1) + "Holberton")    
