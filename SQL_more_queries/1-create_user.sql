d7ad696e14b9a37fff4207d30562e452107b1d1a
Khaalid245 <k.abdillahi@alustudent.com>
2023-11-29 09:04:04 -0800

    1-create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,6 +1,5 @@
--- creates a user and password
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@localhost;
-FLUSH PRIVILEGES;
+-- Create or replace the user `user_0d_1` with the password 'user_0d_1_pwd'
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
+-- Grant all privileges to the user `user_0d_1` on all databases
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
