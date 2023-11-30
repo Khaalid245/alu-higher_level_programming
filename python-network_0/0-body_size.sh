0ca31a0a4c4f987420e469842c99bd505968d2ab
Khaalid245 <k.abdillahi@alustudent.com>
2023-11-30 06:42:07 -0800

    0-body_size.sh

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script that shows the Content-Length from a HTTP request
-curl -sI "$1" | grep "Content-Length:" | cut -d " " -f 2
+#Send request and get the size of the response body in bytes
+curl -sI "$1" | grep 'Content-Length:' | cut -f2 -d' '
