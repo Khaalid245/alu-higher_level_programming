8f7bcad846ec1e93c3e5168dd2db608d051793ff
Khaalid245 <k.abdillahi@alustudent.com>
2023-12-01 20:48:07 +0000

    2-post_email.py

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-"""I documented you"""
+""" A script that sends POST request
+"""
 
-import urllib.request
-import urllib.parse
-import sys
 
 if __name__ == '__main__':
-    """"Documented"""
-    url = sys.argv[1]
-    values = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(values)
-    data = data.encode('ascii')  # data should be bytes
-    req = urllib.request.Request(url, data)
-    with urllib.request.urlopen(req) as response:
-        content = response.read()
-        print("{}".format(content.decode("utf-8")))  
+    from urllib.request import urlopen, Request
+    from urllib.parse import urlencode
+    import sys
+    data = urlencode({"email": sys.argv[2]
+                      }).encode("ascii")
+
+    request = Request(sys.argv[1], data)
+    with urlopen(request) as response:
+        print(response.read().decode('utf-8'))
